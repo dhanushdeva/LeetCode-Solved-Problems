5900a7d186805e9ef7e6c0885edfe8400b0410c6
DHANUSH D <115514596+dhanushdeva@users.noreply.github.com>
2024-08-29 22:37:34 +0530

    Time: 4 ms (42.37%), Space: 44.1 MB (81.54%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0;i<9;i++){
+            HashSet<Integer> hs = new HashSet<>();
+            for(int j=0;j<9;j++){
+                if(hs.contains(board[i][j]+'0')){
+                    return false;
+                }
+                if(board[i][j]!='.'){
+                    hs.add(board[i][j]+'0');
+                }
+            }
+    }
+        for(int i=0;i<9;i++){
+            HashSet<Integer> hs = new HashSet<>();
+            for(int j=0;j<9;j++){
+                if(hs.contains(board[j][i]+'0')){
+                    return false;
+                }
+                if(board[j][i]!='.'){            
+                    hs.add(board[j][i]+'0');
+                }
+            }
+        }
+    
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+                HashSet<Integer> hs = new HashSet<>();
+                for(int k=i;k<i+3;k++){
+                    for(int l=j;l<j+3;l++){
+                        if(hs.contains(board[k][l]+'0')){
+                            return false;
+                        }
+                        if(board[k][l]!='.'){            
+                            hs.add(board[k][l]+'0');
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
