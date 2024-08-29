7863c22586219d0d8eba2f01c01aca4f53b6374a
DHANUSH D <115514596+dhanushdeva@users.noreply.github.com>
2024-08-30 01:35:01 +0530

    Time: 0 ms (100%), Space: 41.2 MB (79.31%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -8,27 +8,24 @@ class Solution {
 }
 
     public int numberOfArithmeticSlices(int[] nums) {
-        // Calculate differences between consecutive elements
         int[] diff = new int[nums.length];
         for (int i = 1; i < nums.length; i++) {
             diff[i] = nums[i] - nums[i - 1];
         }
 
-        int ans = 0; // Total count of arithmetic slices
-        int i = 1, j = 2; // Pointers to scan the diff array
+        int ans = 0; 
+        int i = 1, j = 2; 
 
         while (i != diff.length) {
-            int count = 2; // Number of consecutive elements with the same difference
+            int count = 2; 
             while (j < diff.length && diff[i] == diff[j]) {
                 count++;
                 j++;
             }
 
             if (count >= 3) {
-                // Found an arithmetic slice, count the number of slices within it
                 ans += value(count - 2);
-// WHY count - 2 MAKE a table to see the pattern for set of 3,4,5,6 
-//numbers in sequence and hence get up to it and if any problem comment
+
             }
 
             i = j;
